2b4dd68cf1d92256af25bece8e32adb89f9d36f6
Nh-Kh <180484138+Nh-Kh@users.noreply.github.com>
2026-05-22 10:37:05 -0400

    preliminary separate rules file for rules page

---
+++ client/src/game/monopolyRules.ts
@@ -0,0 +1,12 @@
+export type RuleSection = {
+  title: string;
+  body: string;
+};
+
+// rules section to be visible in the rules page
+export const ruleSections: RuleSection[] = [
+  {
+    title: "Objective",
+    body: "Move around the board duh.",
+  },
+];
